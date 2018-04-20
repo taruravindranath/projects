df98546635669b386067589c75ea85efc775cca5
Tarun <Tarun@172-30-48-183.DYNAPOOL.NYU.EDU>
2018-04-19 22:24:25 -0400

    added shadow to top nav

---
+++ FirebaseBackend/FirebaseBackend/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  FirebaseBackend
+//
+//  Created by Tarun on 4/19/18.
+//  Copyright © 2018 VApp. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ FirebaseBackend/FirebaseBackend/FancyView.swift
@@ -0,0 +1,21 @@
+//
+//  FancyView.swift
+//  FirebaseBackend
+//
+//  Created by Tarun on 4/19/18.
+//  Copyright © 2018 VApp. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        
+    }
+}
