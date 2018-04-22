349bb5a42cadbad40d76018f6b70fefb12415204
Tarun <taruravindranath@gmail.com>
2018-04-22 16:53:56 -0400

    email auth

--- FirebaseBackend/FirebaseBackend/SignInVC.swift
+++ FirebaseBackend/FirebaseBackend/SignInVC.swift
@@ -13,6 +13,10 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: FancyField!
+    
+    @IBOutlet weak var pwdField: FancyField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -51,5 +55,26 @@ class SignInVC: UIViewController {
             }
         })
     }
+    
+    @IBAction func signInBtnPressed(_ sender: Any) {
+        if let email = emailField.text, let pwd = pwdField.text {
+            Auth.auth().signIn(withEmail: email, password: pwd) { (user, error) in
+                if error == nil {
+                    print("FBack: Email user authenticated with Firebase")
+                } else {
+                    Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("FBack: Unable to authenticate with Firebase using email")
+                        } else {
+                            print("FBack: Successfully authenticated with Firebase")
+                        }
+                    })
+                }
+            }
+        }
+        
+        
+    }
+    
 }
 
