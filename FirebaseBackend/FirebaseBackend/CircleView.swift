d4fc73983441797964dd17ef9a15984d89d0df02
Tarun <taruravindranath@gmail.com>
2018-04-22 19:31:00 -0400

    Basic UI

--- FirebaseBackend/FirebaseBackend/CircleView.swift
+++ FirebaseBackend/FirebaseBackend/CircleView.swift
@@ -1,22 +1,26 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  FirebaseBackend
 //
-//  Created by Tarun on 4/19/18.
+//  Created by Tarun on 4/22/18.
 //  Copyright © 2018 VApp. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
