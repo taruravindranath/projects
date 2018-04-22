5b30eb6d17a750a6e51b7be816de49bae78c5297
Tarun <taruravindranath@gmail.com>
2018-04-22 16:25:10 -0400

    fix firebase error

--- FirebaseBackend/FirebaseBackend/AppDelegate.swift
+++ FirebaseBackend/FirebaseBackend/AppDelegate.swift
@@ -26,19 +26,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-        return FBSDKApplicationDelegate.sharedInstance().application(application,
-                                                         open: url,
-                                                         sourceApplication: sourceApplication,
-                                                         annotation: annotation)
+    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any])
+        -> Bool {
+            
+            let shouldOpen :Bool = FBSDKApplicationDelegate.sharedInstance().application(application,  open: url, sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?,annotation:options[UIApplicationOpenURLOptionsKey.annotation])
+          return shouldOpen
+            
     }
-//
-//    func application(_ application: UIApplication,
-//                     open url: URL,
-//                     sourceApplication: String?,
-//                     annotation: Any) -> Bool {
-//        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
-//    }
     
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
