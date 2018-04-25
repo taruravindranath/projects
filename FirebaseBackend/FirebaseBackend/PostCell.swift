2b0bbabeef314d878f119d1f114a8cbf373e751e
Tarun <taruravindranath@gmail.com>
2018-04-24 22:19:15 -0400

    Table view improve looks

---
+++ FirebaseBackend/FirebaseBackend/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  FirebaseBackend
+//
+//  Created by Tarun on 4/24/18.
+//  Copyright © 2018 VApp. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
