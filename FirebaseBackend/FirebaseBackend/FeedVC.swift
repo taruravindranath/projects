0f2dfd27e31feac39ad3491af527cef7c4a2abc2
Tarun <taruravindranath@gmail.com>
2018-04-29 19:02:04 -0400

    Firebase get complete

--- FirebaseBackend/FirebaseBackend/FeedVC.swift
+++ FirebaseBackend/FirebaseBackend/FeedVC.swift
@@ -16,6 +16,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,7 +26,18 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         // listener to listen to db posts table and does changes when db tbl changes
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value as Any)
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, Any> {
+                        let key = snap.key
+                        
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
         
     }
@@ -34,10 +47,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("FBack: \(post.caption)")
+        
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
